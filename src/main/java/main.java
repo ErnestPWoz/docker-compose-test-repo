261f4fa7491f4afb99f5dcc782127edac39c2d78
Ernest Wozniak <ernest.wozniak.se@gmail.com>
2024-10-10 00:28:40 +0200

    change src path

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,9 @@
 public class TestClass // Missing Javadoc comment for class
 {
-    public static void main(String[] args){ // Improper indentation and missing Javadoc for method
+    public static void main(String[]  args)
+    
+    
+    { // Improper indentation and missing Javadoc for method
         int BAD_VARIABLE=10; // Variable name not in camelCase and missing spaces around '='
         for(int i=0;i<10;i++){ // Missing spaces around operators
             System.out.println("Checkstyle test: " + i);
